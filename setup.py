f1d28c5408c60455abe4fad47e2ae6d820b0c536
Geoffrey van Wyk <geoffrey@vanwyk.biz>
2015-05-25 06:57:52 +0200

    Updates setup.py to require Jinja2.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='table_of_contents',
     install_requires=[
         'pyramid',
-        'pyramid_chameleon'
+        'pyramid_jinja2'
     ],
     entry_points="""\
         [paste.app_factory]
